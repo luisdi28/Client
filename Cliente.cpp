1cef99fa8d24d70106a0863828d3fab088d34d87
Luis <luisdi.28@estudiantec.cr>
2022-09-16 19:20:28 -0600

    Initial commit

---
+++ Cliente.cpp
@@ -0,0 +1,5 @@
+//
+// Created by luis on 16/09/22.
+//
+
+#include "Cliente.h"
---
+++ Cliente.h
@@ -0,0 +1,14 @@
+//
+// Created by luis on 16/09/22.
+//
+
+#ifndef CLIENT_CLIENTE_H
+#define CLIENT_CLIENTE_H
+
+
+class Cliente {
+
+};
+
+
+#endif //CLIENT_CLIENTE_H
